63f974b15e44b30e80181fe8256550c1e42a4273
Desiree Nelson <palebluedot4@gmail.com>
2021-11-15 12:52:23 -0500

    tinkered with css on the main page. Updated the navbar to add links to other aspects of the site

---
+++ src/components/navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+//import { FaBars } from 'react-icons/fa';
+import '../styles/style.scss';
+
+const Navbar = () => {
+  return (
+    <nav id={'navbar'}>
+      <a href='https://sites.google.com/site/ryanlab1/Home' id={'lab-name'}>
+        <h1> Ryan Lab </h1>
+      </a>
+      <section id={'nav-links'}>
+        <a>
+          <h3>Research</h3>
+        </a>
+        <a href='https://sites.google.com/site/ryanlab1/Home/people?authuser=0'>
+          <h3>People</h3>
+        </a>
+        <a href='https://sites.google.com/site/ryanlab1/Home/publications?authuser=0'>
+          <h3>Publications</h3>
+        </a>
+        <a href='https://sites.google.com/site/ryanlab1/Home/useful-links?authuser=0'>
+          <h3>Links</h3>
+        </a>
+        <a href='https://sites.google.com/site/ryanlab1/Home/contact?authuser=0'>
+          <h3>Contact</h3>
+        </a>
+      </section>
+
+      {/* <div></div>
+      <FaBars className={'Bars'} /> */}
+    </nav>
+  );
+};
+
+export default Navbar;
