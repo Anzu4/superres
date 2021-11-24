2a751530acf19823ed93dbb77c9744a25fdc02be
Desiree Nelson <palebluedot4@gmail.com>
2021-11-24 17:54:59 -0500

    changed some sections from felx to grid. Added text in Section 2. Added images and grid to Section 3

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Three } from '.';
 import '../styles/style.scss';
 
 export default function MainPage() {
@@ -25,53 +24,38 @@ export default function MainPage() {
           <span className={'span-1'}>t</span>ion
         </section>
       </div>
-      {/* More detail can go here. A few paragraphs expanding on the first section */}
+      {/* More detail can go here. A few paragraphs expanding on the first section. Links to further explainations and power point images */}
       <div id={'second-box'}>
         <section>
           <h1>Section Header</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-            bibendum massa. Proin sagittis augue sed lectus pellentesque gravida
-            vel sed nibh. Donec vel justo porttitor, bibendum ante et, tempor
-            libero. Orci varius natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus. Nulla id volutpat ante. Nam
-            dignissim, sapien et congue pharetra, ipsum ligula fermentum dui, eu
-            interdum eros purus eget nisi. Morbi auctor lacinia sapien auctor
-            maximus. Suspendisse potenti. Praesent metus lorem, ullamcorper ac
-            tempus at, egestas nec nisl. Vivamus ac ex lectus. Donec dignissim
-            magna maximus est scelerisque viverra et vel augue. Nunc fermentum
-            tristique odio.{' '}
-          </p>
-          <p>
-            {' '}
-            Nam tincidunt risus mi, sit amet accumsan dui tristique et. Maecenas
-            eu lacus eu nisl sagittis egestas. Sed quis erat dignissim, semper
-            eros et, ultrices elit. Nulla facilisi. Nunc lacus turpis, porttitor
-            sed gravida sit amet, porttitor nec sapien. Nam faucibus felis eget
-            vulputate hendrerit. Curabitur placerat cursus congue.{' '}
-          </p>{' '}
-          <p>
-            Duis dictum ex vel mauris ornare, at faucibus purus maximus. Aenean
-            ut rhoncus sapien. Nunc venenatis consequat ante vel commodo. Donec
-            lacinia dui sed arcu bibendum, nec faucibus sapien vestibulum. Proin
-            posuere nibh vel risus ultricies hendrerit. Nullam turpis velit,
-            hendrerit at dui non, sagittis posuere justo. Aliquam vel arcu
-            auctor, facilisis sem a, blandit tellus. Phasellus aliquam rutrum
-            mi, id auctor ipsum suscipit facilisis. Etiam sed mauris tristique
-            metus fringilla tempus. Etiam aliquet ante nec ultrices egestas.
-            Integer posuere pellentesque eleifend. Vivamus tortor velit,
-            consectetur sed turpis at, dictum luctus eros. Proin cursus, odio
-            non ullamcorper elementum, neque nisi hendrerit purus, sed tempus
-            mauris nisi at est. Cras aliquet dui mi, in tincidunt tellus laoreet
-            quis. Nullam mollis egestas sapien, et iaculis tellus tristique sed.
+            The presynaptic bouton is a core information processing unit in
+            neural circuits. Understanding the rules that govern presynaptic
+            behavior requires knowing not only which proteins are important, but
+            where they are organized within the bouton. We use{' '}
+            <span className='popout microscopy'>
+              {' '}
+              advanced microscopy techniques{' '}
+            </span>{' '}
+            to pinpoint where molecules are organized in live presynaptic
+            boutons. By getting a clearer picture of molecular organization in
+            live synapses, we can expand our understanding of presynaptic
+            dynamics.{' '}
           </p>
         </section>
       </div>
       {/* Final section. Might include links to relevent info? */}
       <div id={'third-box'}>
         <section>
-          <h1>Section Header</h1>
-          <Three />
+          <h1 className='header'>Section Header</h1>
+          <div className='img-left' id='vGlut'></div>
+          <p className='info-right'>
+            This is information about the image to the left
+          </p>
+          <p className='info-left'>
+            This is information about the image to the right
+          </p>
+          <div className='img-right' id='sunrise'></div>
         </section>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import Navbar from './navbar';
 import MainPage from './MainPage';
-import Three from './three';
 
-export { Navbar, MainPage, Three };
+export { Navbar, MainPage };
